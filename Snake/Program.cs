76e7bb9ea262b1c7f2486618297adf0e60726515
Sergey <serezha.cruckowsckij@yandex.ru>
2020-11-22 18:55:25 +0700

    Дополнение к предыдущему комиту (Разметка области, создание класса Фигура и наследование от него классов линий (оптимизация кода))

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -9,13 +9,13 @@ namespace Snake
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
             HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
-            upline.Draw();
+            upline.Drow();
             HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
-            downline.Draw();
+            downline.Drow();
             VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
-            leftline.Draw();
+            leftline.Drow();
             VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
-            rightline.Draw();
+            rightline.Drow();
 
             Point d = new Point(1, 3, '*');
             d.Draw();
