b30722b9f555de7a893e3701e2b0ccf9cdaec033
Sergey <serezha.cruckowsckij@yandex.ru>
2020-11-22 11:14:26 +0700

    Добавление функции Draw()

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -4,9 +4,23 @@ namespace Snake
 {
     class Program
     {
+        static void Draw (int x, int y, char c)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char c = '*';
+            Draw(x1, y1, c);
+
+            int x2 = 5;
+            int y2 = 2;
+            char c2 = '#';
+            Draw(x2, y2, c2);
+
             Console.ReadLine();
         }
     }
